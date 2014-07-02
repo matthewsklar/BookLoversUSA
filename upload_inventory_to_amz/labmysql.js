3f0d5eb0255560d21c3b631263bce24106d4b342
root <root@vm-0.dfsklar.koding.kd.io>
2014-07-02 04:59:05 +0000

    add

---
+++ upload_inventory_to_amz/labmysql.js
@@ -0,0 +1,10 @@
+var mysql = require('mysql'); 
+
+var connection = mysql.createConnection({
+    host: 'tsunami2014.cwj0cl8a8xkt.us-west-2.rds.amazonaws.com',
+    user: 'admin',
+    password: 'ioi3JFI3lsne42'
+});
+
+connection.connect();
+
